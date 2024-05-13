9aa2f81da9adc68450ad9b71d04c77c5c55240ad
加藤嵩大 <takahiro@katoushiori-no-MacBook-Air.local>
2024-05-13 18:07:28 +0900

    TEST

--- simulation.py
+++ simulation.py
@@ -13,7 +13,7 @@ def calculate_index(player_hand, dealer_hand):
         print("Bet Insurance")
     print()
     index = ((player_hand % 11) * 13 % len(model)) - 1
-    index += dealer_hand * 13 
+    index += (dealer_hand + 10) * 13 # 基本的にディーラーが伏せているカードは10という前提
     index %= len(model)
 
     # best_strategyのインデックスに対応する行動を取得
